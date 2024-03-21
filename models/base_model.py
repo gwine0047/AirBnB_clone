266a1d1c6255df98905c36e706d3a777f1becec5
Okoeguale Godwin <okoegualegodwin@gmail.com>
2024-03-21 19:53:59 +0100

    add test_base_model file and some empty files

--- models/base_model.py
+++ models/base_model.py
@@ -3,7 +3,7 @@
 
 import uuid
 from datetime import datetime
-from models import storage
+# from models import storage
 
 
 class BaseModel:
@@ -32,4 +32,24 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
+            # storage.new(self)
+
+    def __str__(self):
+        """This helps to return the official string representation"""
+
+        return f"[{type(self).__name__}] ({self.id}) {self.__dict__}"
+
+    def save(self):
+        """updates the attribute self.update_at"""
+
+        self.updated_at = datetime.now()
+        # storage.save
+
+    def to_dict(self):
+        """turns __dict__ to a regular python dictionary"""
+
+        inst_dict = self.__dict__.copy()
+        inst_dict["__class__"] = type(self).__name__
+        inst_dict["updated_at"] =  inst_dict["updated_at"].isoformat()
+        inst_dict["created_at"] =  inst_dict["created_at"].isoformat()
+        return inst_dict
